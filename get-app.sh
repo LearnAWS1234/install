01263f8631f7cc73e4c5ad99b2e87fabfe837e69
LearnAWS1234 <119797788+LearnAWS1234@users.noreply.github.com>
2022-12-23 11:23:47 +0000

    Update get-app.sh

--- get-app.sh
+++ get-app.sh
@@ -3,7 +3,8 @@ ARGS=$@
 _9HITSUSER="_9hits"
 
 #DOWNLOAD_URL="http://dl.9hits.com/9hitsv3-linux64.tar.bz2"
-DOWNLOAD_URL="https://www.dropbox.com/s/y10fgiygbtj1e9n/9hitsv3-linux64.tar.bz2?dl=1"
+#DOWNLOAD_URL="https://www.dropbox.com/s/y10fgiygbtj1e9n/9hitsv3-linux64.tar.bz2?dl=1"
+DOWNLOAD_URL="http://buy9hits.com/dl/9hitsv3-linux64.tar.bz2"
 
 CURRENT_HASH=$(date +%s)
 TOKEN=""
